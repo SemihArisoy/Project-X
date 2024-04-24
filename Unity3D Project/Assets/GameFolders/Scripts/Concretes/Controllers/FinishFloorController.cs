57bd2e3c36d7d29dbb4774378dc580f8fd41dcdc
İbrahim Semih Arısoy <semiharisoy80@gmail.com>
2024-04-24 14:50:42 +0300

    FinishFloorController
    FinishFloorController eklendi gameobject ustune fx'ler verildi oyun bittiginde fx'ler calisir.

--- Unity3D Project/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
+++ Unity3D Project/Assets/GameFolders/Scripts/Concretes/Controllers/FinishFloorController.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 namespace Unity3DProject.Controllers
 {
     public class FinishFloorController : MonoBehaviour
     {
+        [SerializeField] GameObject _finishFireWork;
+        [SerializeField] GameObject _finishLight;
 
+        private void OnCollisionEnter(Collision other)
+        {
+            PlayerController player = other.collider.GetComponent<PlayerController>();
+
+            if (player == null) return;
+
+            if(other.GetContact(0).normal.y == -1)
+            {
+                _finishFireWork.gameObject.SetActive(true);
+                _finishLight.gameObject.SetActive(true);
+            }
+            else
+            {
+                //Game Over
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 }
 
