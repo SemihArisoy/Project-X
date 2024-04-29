116197de3934d1907bb1427341d59a2e96600a4c
İbrahim Semih Arısoy <semiharisoy80@gmail.com>
2024-04-29 12:29:48 +0300

    GameOver Panel
    GameOver panel yandiktan sonra cikar

---
+++ Unity3D Project/Assets/GameFolders/Scripts/Concretes/UIs/GameOverObject.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity3DProject.Managers;
+using UnityEngine;
+
+namespace Unity3DProject.UIs
+{
+    public class GameOverObject : MonoBehaviour
+    {
+        [SerializeField] GameObject _gameOverPanel;
+
+        private void Awake()
+        {
+            if(_gameOverPanel.activeSelf) 
+            {
+                _gameOverPanel.SetActive(false);
+            }
+        }
+
+        private void OnEnable()
+        {
+            GameManager.Instance.OnGameOver += HandleOnGameOver;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.Instance.OnGameOver -= HandleOnGameOver;
+        }
+
+        private void HandleOnGameOver()
+        {
+            if (!_gameOverPanel.activeSelf)
+            {
+                _gameOverPanel.SetActive(true);
+            }
+        }
+    }
+}
