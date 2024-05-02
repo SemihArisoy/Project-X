391763d228696fa5356dfc0a587460e6af9345ef
İbrahim Semih Arısoy <semiharisoy80@gmail.com>
2024-05-02 13:47:55 +0300

    Level Design
    3 adet level olusturuldu, Refuel Station Floor eklendi. Roketin benzini azaldiginda Refuel Station Floor uzerine durarak benzin doldurabilir.

--- Unity3D Project/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
+++ Unity3D Project/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
@@ -8,10 +8,10 @@ namespace Unity3DProject.Controllers
     public class MoverWallController : WallController
     {
         [SerializeField] Vector3 _direction;
-        [SerializeField] float _factor;
         [SerializeField] float _speed = 1f;
 
         Vector3 _startPosition;
+        float _factor;
         const float FULL_CIRCLE = Mathf.PI * 2f;
 
 
@@ -25,7 +25,7 @@ namespace Unity3DProject.Controllers
             float cycle = Time.time / _speed;
             float sinWave = Mathf.Sin(cycle * FULL_CIRCLE);
 
-            _factor = Mathf.Abs(sinWave);
+            _factor = sinWave / 2f + 0.5f;
 
             Vector3 offset = _direction * _factor;
             transform.position = offset + _startPosition;
